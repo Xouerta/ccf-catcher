1cc1f6c91afbb359ce2805274f1bff923b8d4eda
src <2659279285@qq.com>
2025-03-27 18:52:15 +0800

    infrastructure(sercurity_ai): 更新 Elasticsearch 和 API 服务器地址
    - 修改 Elasticsearch 地址从 100.118.110.15:9200 到 192.168.1.7:9200
    - 更新 API 服务器地址从 http://localhost:6000 到 http://192.168.1.7:5500/predict
    - 在 SecurityAiApplication 中添加 @EnableAsync 注解以支持异步处理

--- sercurity_ai/src/main/java/com/ccf/sercurity/SecurityAiApplication.java
+++ sercurity_ai/src/main/java/com/ccf/sercurity/SecurityAiApplication.java
@@ -3,7 +3,9 @@ package com.ccf.sercurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @EnableElasticsearchRepositories
 public class SecurityAiApplication {
