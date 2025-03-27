f740241e2b0669ba36b99694f515b31896fc35fe
sudorm <2659279285@qq.com>
2025-03-27 12:40:36 +0800

    feat: 添加日志控制器
    - 新增 LogsController 类，用于处理日志相关的请求
    - 设置控制器的基路径为 /logs
    - 使用 Lombok 的 @RequiredArgsConstructor 注解简化构造函数

---
+++ sercurity_ai/src/main/java/com/ccf/sercurity/controller/LogsController.java
@@ -0,0 +1,12 @@
+package com.ccf.sercurity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/logs")
+public class LogsController {
+
+}
