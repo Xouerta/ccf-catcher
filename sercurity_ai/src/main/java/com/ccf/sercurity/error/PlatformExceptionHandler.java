ec9e963656f518180a179579bc8cd914cd3052cc
sudorm <2659279285@qq.com>
2025-04-04 18:09:38 +0800

    fix(sercurity): 添加对不支持的媒体类型异常的处理- 在 PlatformExceptionHandler 类中添加了对 HttpMediaTypeNotSupportedException 的处理方法- 当发生不支持的媒体类型异常时，返回 400 错误请求响应 -此修改提高了系统的健壮性，能够更好地处理客户端发送的不支持的媒体类型请求

--- sercurity_ai/src/main/java/com/ccf/sercurity/error/PlatformExceptionHandler.java
+++ sercurity_ai/src/main/java/com/ccf/sercurity/error/PlatformExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -62,6 +63,11 @@ public class PlatformExceptionHandler {
 
         log.warn(buffer.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorResult> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResult(400, e.getMessage()));
     }
 
     @ExceptionHandler(HandlerMethodValidationException.class)
