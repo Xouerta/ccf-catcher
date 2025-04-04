2a43c31f53437bb0dde676734c8deae1c3eff8a0
sudorm <2659279285@qq.com>
2025-04-04 18:09:48 +0800

    refactor(sercurity_ai):重构 NetworkMessageListener 类
    - 添加了 TrafficAnalysisService 的依赖
    - 更新了 listen 方法，将消息传递给 TrafficAnalysisService 处理- 引入了 @RequiredArgsConstructor 注解以自动生成构造函数

--- sercurity_ai/src/main/java/com/ccf/sercurity/message/NetworkMessageListener.java
+++ sercurity_ai/src/main/java/com/ccf/sercurity/message/NetworkMessageListener.java
@@ -1,12 +1,18 @@
 package com.ccf.sercurity.message;
 
+import com.ccf.sercurity.service.TrafficAnalysisService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class NetworkMessageListener {
+
+    private final TrafficAnalysisService trafficAnalysisService;
+
     @KafkaListener(topics = "network_predictions")
-    public void listen(String message) {
-        System.out.println("Received Messasge in group network: " + message);
+    public void listen(String message) throws Exception {
+        trafficAnalysisService.saveTrafficData(message);
     }
 }
